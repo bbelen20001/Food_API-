ff9500261864996cbb739d87f5f6de1575b59bfe
bbelen20001 <belenbrenda10@gmail.com>
2023-03-02 23:10:20 -0400

    todo ok deploy

--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,
 } = process.env;
-
+postgres://dngjudlg:0UOKsSW-pn5AAfGpWKfeG1BWH3J5ZHCi@mahmud.db.elephantsql.com/dngjudlg
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
